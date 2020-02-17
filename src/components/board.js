8d006958385f14109d9a8546e46919dc7731f1f8
Tom Kwong <tk3369@gmail.com>
2020-02-17 13:32:17 -0800

    added currentPlayer state to Board and passing it to all Square components

--- src/components/board.js
+++ src/components/board.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './square';
 
 export default function Board() {
+
+  const [currentPlayer, setCurrentPlayer]= useState('X');
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square index="0" />
-        <Square index="1" />
-        <Square index="2" />
+        <Square index="0" currentPlayer={currentPlayer} />
+        <Square index="1" currentPlayer={currentPlayer} />
+        <Square index="2" currentPlayer={currentPlayer} />
       </div>
       <div className="board-row">
-        <Square index="3" />
-        <Square index="4" />
-        <Square index="5" />
+        <Square index="3" currentPlayer={currentPlayer} />
+        <Square index="4" currentPlayer={currentPlayer} />
+        <Square index="5" currentPlayer={currentPlayer} />
       </div>
       <div className="board-row">
-        <Square index="6" />
-        <Square index="7" />
-        <Square index="8" />
+        <Square index="6" currentPlayer={currentPlayer} />
+        <Square index="7" currentPlayer={currentPlayer} />
+        <Square index="8" currentPlayer={currentPlayer} />
       </div>
     </div>
   )
--- src/components/square.js
+++ src/components/square.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 // Construct a Square component with position `i`.
 // Note: React Hook function names MUST BE capitalized
-export default function Square({index}) {
+export default function Square({index, currentPlayer}) {
 
   // The `fill` state should be a string - 'X', 'O', or ''
   const [ fill, setFill ] = useState('');
 
   const handleClick = () => {
-    setFill(index.toString());
+    setFill(currentPlayer);
   };
 
   return (
