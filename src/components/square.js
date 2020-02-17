a66c19a2e42da98c01ff65efb93663c38adb7bbb
Tom Kwong <tk3369@gmail.com>
2020-02-17 13:26:53 -0800

    use shorthand for passing properties

--- src/components/square.js
+++ src/components/square.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 
 // Construct a Square component with position `i`.
 // Note: React Hook function names MUST BE capitalized
-export default function Square(props) {
+export default function Square({index}) {
 
   // The `fill` state should be a string - 'X', 'O', or ''
-  const [ fill, setFill ] = useState(props.index.toString());
+  const [ fill, setFill ] = useState(index.toString());
 
   return (
     // key must be specified of else the Board component would complain
     // what to do when clicked?  need context -- who's current player.
-    <button key={props.index} className="square" onClick={() => setFill('')}>
+    <button key={index} className="square" onClick={() => setFill('')}>
       {fill}
     </button>
   )
