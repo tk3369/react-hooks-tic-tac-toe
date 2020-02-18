6d03b8fdb6b0351e3b49396329246d2f47036ded
Tom Kwong <tk3369@gmail.com>
2020-02-17 18:37:17 -0800

    Play with effect... only when history changes

--- src/components/board.js
+++ src/components/board.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Square from './square';
 
 export default function Board() {
@@ -12,12 +12,17 @@ export default function Board() {
 
   const [ history, setHistory ] = useState([]);
 
+  // 2nd argument is an array of state objects to watch
+  useEffect(() => {
+    console.log("useEffect called.  History: " + history.join(", "));
+  }, [history])
+
   return (
     <div className="board">
       <div className="info">
         Current player: {currentPlayer}
         <p></p>
-        History: {history.join(", ")} 
+        History: {history.join(", ")}
       </div>
       <div className="board-row">
         <Square index="0" currentPlayer={currentPlayer} advance={advance} />
